4898a4fe7d28fe3f0c799fdbdec65c85d20ec72f
shadowzilot <e.ponomaryow2017@yandex.ru>
2021-11-21 18:19:32 +0300

    Added red marking for negative money rest

--- app/src/main/java/com/human_developing_soft/productcalc/product_storage/ui/MoneyInfoView.kt
+++ app/src/main/java/com/human_developing_soft/productcalc/product_storage/ui/MoneyInfoView.kt
@@ -1,5 +1,9 @@
 package com.human_developing_soft.productcalc.product_storage.ui
 
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import com.human_developing_soft.productcalc.R
 import com.human_developing_soft.productcalc.databinding.ProductsBudgetInfoBinding
 import com.human_developing_soft.productcalc.product_storage.StringContext
@@ -26,10 +30,20 @@ interface MoneyInfoView : DayUi.Mapper<Unit> {
         override fun map(id: Int, money: Int, moneyRest: Int, productSumma: Int) {
             mBinding.apply {
                 val strings = StringContext.Base(this.root.context)
-                this.allMoneyView.text = strings.string(R.string.all_money, money)
+                val spannable = SpannableString(
+                    "${strings.string(R.string.rest_money)} $moneyRest"
+                )
+                if (moneyRest < 0) {
+                    spannable.setSpan(ForegroundColorSpan(Color.RED),
+                    strings.string(R.string.rest_money).length,
+                        spannable.length,
+                        Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+                }
+                this.allMoneyView.text = strings.string(R.string.all_money,
+                    money)
                 this.wastedMoneyView.text = strings.string(R.string.wasted_money,
                     productSumma)
-                this.restMoneyView.text = strings.string(R.string.rest_money, moneyRest)
+                this.restMoneyView.text = spannable
             }
         }
     }
